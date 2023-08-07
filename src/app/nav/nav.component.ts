65696063198b45b02fbe69b69cd6a75f75dbadce
marci <kismarczirobi@gmail.com>
2023-08-07 20:34:20 +0200

    basic login

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AccountService } from '../services/account.service';
 
 @Component({
   selector: 'app-nav',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
+  model: any = {};
+  loggedIn = false;
 
+  constructor(private accountService: AccountService) { }
+
+
+  login(){
+    this.accountService.login(this.model).subscribe({
+      next: response => {
+        console.log(response);
+        this.loggedIn = true;
+      },
+      error: error => console.log(error)
+    });
+  }
 }
