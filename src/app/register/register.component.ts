64fcb596ee5cd52277676b99859c98feba05715a
marci <kismarczirobi@gmail.com>
2023-08-08 17:58:44 +0200

    Add home page and register form

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+  model: any = {};
+
+  constructor() { }
+
+  register() {
+    console.log(this.model);
+  }
+
+  cancel() {
+    console.log('cancelled');
+  }
+}
