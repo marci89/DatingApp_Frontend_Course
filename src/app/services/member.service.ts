a1f13afcf0aae04b95df477856746178222e1eff
marci <kismarczirobi@gmail.com>
2023-08-11 09:07:55 +0200

    Interceptor for jwt token

--- src/app/services/member.service.ts
+++ src/app/services/member.service.ts
@@ -12,23 +12,10 @@ export class MemberService {
   constructor(private http: HttpClient) { }
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'user', this.getHttpOptions());
+    return this.http.get<Member[]>(this.baseUrl + 'user');
   }
 
   getMember(username: string){
-    return this.http.get<Member>(this.baseUrl + 'user/' + username, this.getHttpOptions());
-  }
-
-  getHttpOptions() {
-    const userString = localStorage.getItem('user');
-    if(!userString) return;
-
-    const user = JSON.parse(userString);
-
-    return {
-      headers: new HttpHeaders({
-        Authorization: 'Bearer ' + user.token
-      })
-    }
+    return this.http.get<Member>(this.baseUrl + 'user/' + username);
   }
 }
