bcbbfccd991c8885fd473057d486df636afb1399
marci <kismarczirobi@gmail.com>
2023-08-11 12:46:04 +0200

    Member details page

--- src/app/members/member-detail/member-detail.component.ts
+++ src/app/members/member-detail/member-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Member } from 'src/app/models/member';
+import { MemberService } from 'src/app/services/member.service';
 
 @Component({
   selector: 'app-member-detail',
@@ -6,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./member-detail.component.css']
 })
 export class MemberDetailComponent {
+  member: Member | undefined;
 
+  constructor(private memberService: MemberService, private route: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember() {
+    const username = this.route.snapshot.paramMap.get('username');
+    if (!username) return;
+
+    this.memberService.getMember(username).subscribe({
+      next: member => this.member = member
+    });
+  }
 }
