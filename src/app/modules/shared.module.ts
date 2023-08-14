b3f947592ca83346290348a6e18095bda4528601
marci <kismarczirobi@gmail.com>
2023-08-14 10:48:28 +0200

    Using the angular bootstrap pagination component

--- src/app/modules/shared.module.ts
+++ src/app/modules/shared.module.ts
@@ -6,6 +6,7 @@ import { TabsModule } from 'ngx-bootstrap/tabs';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { FileUploadModule } from 'ng2-file-upload';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 
 
@@ -23,6 +24,7 @@ import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
     }),
     FileUploadModule,
     BsDatepickerModule.forRoot(),
+    PaginationModule.forRoot(),
   ],
   exports: [
     BsDropdownModule,
@@ -30,7 +32,8 @@ import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
     TabsModule,
     NgxSpinnerModule,
     FileUploadModule,
-    BsDatepickerModule
+    BsDatepickerModule,
+    PaginationModule
   ]
 
 })
