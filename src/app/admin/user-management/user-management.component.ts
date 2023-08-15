361757e57446cb4740dca206b19a75e856671f8a
marci <kismarczirobi@gmail.com>
2023-08-15 21:47:59 +0200

    Add modal and editing roles

--- src/app/admin/user-management/user-management.component.ts
+++ src/app/admin/user-management/user-management.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { BsModalRef, BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
 import { User } from 'src/app/models/user';
 import { AdminService } from 'src/app/services/admin.service';
+import { RolesModalComponent } from 'src/app/modals/roles-modal/roles-modal.component';
 
 @Component({
   selector: 'app-user-management',
@@ -9,8 +11,14 @@ import { AdminService } from 'src/app/services/admin.service';
 })
 export class UserManagementComponent implements OnInit {
   users: any[] = [];
+  bsModalRef: BsModalRef<RolesModalComponent> = new BsModalRef<RolesModalComponent>();
+  availableRoles = [
+    'Admin',
+    'Moderator',
+    'Member'
+  ]
 
-  constructor(private adminService: AdminService) { }
+  constructor(private adminService: AdminService, private modalService: BsModalService) { }
 
   ngOnInit(): void {
     this.getUsersWithRoles()
@@ -20,5 +28,31 @@ export class UserManagementComponent implements OnInit {
     this.adminService.getUsersWithRoles().subscribe({
       next: users => this.users = users
     })
+  }
+
+  openRolesModal(user: any) {
+    const config = {
+      class: 'modal-dialog-centered',
+      initialState: {
+        username: user.username,
+        availableRoles: this.availableRoles,
+        selectedRoles: [...user.roles]
+      }
+    }
+    this.bsModalRef = this.modalService.show(RolesModalComponent, config);
+    this.bsModalRef.onHide?.subscribe({
+      next: () => {
+        const selectedRoles = this.bsModalRef.content?.selectedRoles;
+        if (!this.arrayEqual(selectedRoles, user.roles)) {
+          this.adminService.updateUserRoles(user.username, selectedRoles!).subscribe({
+            next: roles => user.roles = roles
+          })
+        }
+      }
+    })
+  }
+
+  private arrayEqual(arr1: any, arr2: any) {
+    return JSON.stringify(arr1.sort()) === JSON.stringify(arr2.sort())
   }
 }
