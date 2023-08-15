c0ae1768891bc781af0de95bae893b61aa767f99
marci <kismarczirobi@gmail.com>
2023-08-15 12:26:07 +0200

    Setting up for messages

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Message } from '../models/message';
+import { Pagination } from '../models/pagination';
+import { MessageService } from '../services/messages.service';
 
 @Component({
   selector: 'app-messages',
@@ -6,5 +9,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent {
+  messages?: Message[] = [];
+  pagination?: Pagination;
+  container = "Unread";
+  pageNumber = 1;
+  pageSize = 5;
 
+  constructor(private messageService: MessageService) { }
+
+  ngOnInit(): void {
+    this.loadMessages();
+  }
+
+  loadMessages() {
+    this.messageService.getMessages(this.pageNumber, this.pageSize, this.container).subscribe({
+      next: response => {
+        this.messages = response.result;
+        this.pagination = response.pagination;
+      }
+    })
+  }
+
+  pageChanged(event: any) {
+    if (this.pageNumber !== event.page) {
+      this.pageNumber = event.page;
+      this.loadMessages();
+    }
+  }
 }
